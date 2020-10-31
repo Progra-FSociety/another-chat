c25cf46c797750c2ab111fdcdf3ba16cc216a0a9
Leandro Tebes <leandro.tebes@mercadolibre.com>
2020-10-31 01:15:00 -0300

    Added send private command logic.

--- src/servidor/SendPrivate.java
+++ src/servidor/SendPrivate.java
@@ -1,22 +1,47 @@
 package servidor;
 
-import java.util.Iterator;
+import entities.DataTransferDto;
+import entities.Message;
+
+import java.io.IOException;
 import java.util.List;
 
 public class SendPrivate extends Command {
 
-	private List<Chat> chats;
-	static List<ClientListener> clients;
-	public SendPrivate(List<ClientListener> clients, List<Chat> chats) {
+	private final List<Chat> chats;
+	private final String userName;
+	private final List<ClientListener> clients;
+	private final ClientListener client;
+	private final Message message;
+
+	public SendPrivate(final List<ClientListener> clients,
+					   final List<Chat> chats,
+					   final String userName,
+					   final ClientListener client,
+					   final Message message) {
 		this.chats = chats;
 		this.clients = clients;
+		this.userName = userName;
+		this.client = client;
+		this.message = message;
 	}
 
-	public  void execute() {
-		for (Chat chat : chats) {
-			//if(chat.getUsers())
+	public  void execute() throws IOException {
+		String clienteHacia = message.getChat();
+		Message msg;
+		ClientListener cliente = clients.stream()
+				.filter(x -> x.getName().equals(clienteHacia))
+				.findFirst().orElse(null);
+
+		if(cliente != null){
+			msg = new Message(client.getName(), message.getMessage(), message.getChat() + " (t√∫)" );
+		} else{
+			// en el caso de no poder entregar el mensaje le notifico al wachin
+			msg = new Message(client.getName(), "No pudo se entregado tu mensaje a: " + clienteHacia, client.getName());
+			cliente = client;
 		}
-		
-		//Ac· uso los atributos para hacer la magia.
+		DataTransferDto dto = new DataTransferDto(msg);
+		String json = gsonHelper.toJson(dto);
+		cliente.getOutput().writeObject(json);
 	}
 }
